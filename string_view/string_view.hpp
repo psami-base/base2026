1edc551a124282094448c0088dfd58934557098b
Ibragimov Bulat <ibrbulat@yandex.ru>
2026-04-15 22:20:31 +0300

    add StringViewOutOfRange

--- string_view/string_view.hpp
+++ string_view/string_view.hpp
@@ -1,1 +1,7 @@
-// YOUR CODE ...
+#include <stdexcept>
+
+class StringViewOutOfRange : public std::out_of_range {
+ public:
+  StringViewOutOfRange() : std::out_of_range("StringViewOutOfRange") {
+  }
+};
